ee2498ba659bb94df8f630c83f7750acd0a66992
joshuacolin <joshuacolin@qualtrics.com>
2024-09-03 16:22:10 -0600

    Updating script to accept a list of characaters

--- SpecialCharsCalc.py
+++ SpecialCharsCalc.py
@@ -5,20 +5,20 @@ def UnsupportedCharsTable(df, unsupportedChars, required_columns):
     correctedUnsopportedChars = '[' + unsupportedChars.replace('[', '\[').replace(']', '\]') + ']'
     specialCharsTable = {}
     for colName in required_columns:
-        notEmptyRows = []
-        specialChars = []
-        df[colName] = df[colName].astype(str)
-        notEmptyRows = df[(df[colName].notna()) & (df[colName].notnull())]
-        specialChars = notEmptyRows[notEmptyRows[colName].str.contains(correctedUnsopportedChars)]
+        if colName != 'Email':
+            notEmptyRows = []
+            specialChars = []
+            df[colName] = df[colName].astype(str)
+            notEmptyRows = df[(df[colName].notna()) & (df[colName].notnull())]
+            specialChars = notEmptyRows[notEmptyRows[colName].str.contains(correctedUnsopportedChars)]
 
-        if (len(specialChars) == 0):
-            specialChars = pd.DataFrame(columns=['There are no unsupported characters in this column.'])
-            specialCharsTable[colName] = specialChars
-        else:
-            if colName == 'Unique Identifier':
-                specialCharsTable[colName] = specialChars[[colName]]
+            if (len(specialChars) == 0):
+                specialChars = pd.DataFrame(columns=['There are no unsupported characters in this column.'])
+                specialCharsTable[colName] = specialChars
             else:
-                specialCharsTable[colName] = specialChars[['Unique Identifier', colName]]
-    
+                if colName == 'Unique Identifier':
+                    specialCharsTable[colName] = specialChars[[colName]]
+                else:
+                    specialCharsTable[colName] = specialChars[['Unique Identifier', colName]]
     return specialCharsTable
         
